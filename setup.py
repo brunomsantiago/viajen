3247b897d71a659118f5c67f2d53f1ff68be601e
Bruno Santiago <brunomsantiago@gmail.com>
2021-03-10 11:10:35 -0300

    packaging

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='viajen',
+      version='0.1',
+      description=('View Images as Animation in Jupyter and Equivalent'
+                   ' Notebooks'),
+      url='',
+      author='Bruno Santiago',
+      license='',
+      packages=['viajen'])
